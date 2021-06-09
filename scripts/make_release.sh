d342c0eb271545198ce86d9bb333cb17e65443a5
Jeremy Castagno <JeremyBYU@gmail.com>
2021-06-08 21:48:16 -0400

    Merging S2Beta into Master

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 python scripts/manage_versions.py --bump patch
-pip install .
+pip install . --use-feature=in-tree-build
 cd src_docs && python make_docs.py && cd ..
 git status
 
